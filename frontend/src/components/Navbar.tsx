11e077d61f4ec6059484a91cc0f585c6a08865de
Kevin Truong <kevin97truong@gmail.com>
2026-05-11 07:31:03 -0700

    Fix frontend build

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import {NavLink} from "react-router"
 function Navbar () {
     return (
         <nav className={'border-b'}>
-            <div className={'mx-auto flex wax-w-7xl items-center justify-between px-8 py-4'}>
+            <div className={'mx-auto flex max-w-7xl items-center justify-between px-8 py-4'}>
             <div className={'text-3xl font-bold tracking-tight'}>VisualRaindrop</div>
                 <div className={'flex items-center gap-10 text-sm font-semibold tracking-tight'}>
                     <NavLink to={'#home'} className={'hover:text-purple-500'}>Home</NavLink>
