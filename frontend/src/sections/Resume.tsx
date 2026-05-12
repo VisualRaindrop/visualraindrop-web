1d6092b29950fb578e8092006089c96850c6b53d
Kevin Truong <kevin97truong@gmail.com>
2026-05-12 08:24:21 -0700

    Add UI to timeline

--- frontend/src/sections/Resume.tsx
+++ frontend/src/sections/Resume.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { getTimeline  } from "../api/resume_api";
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 interface Timeline {
     type: 'experience' | 'education';
@@ -19,18 +21,30 @@ function Resume() {
     }, []);
     return(
         <section className={'min-h-screen'}>
-            {timeline.map((occupation, index) => (
-                <div key={index}>
-                    <h3 className={'text-xl mt-4 font-bold'}>{occupation.title}</h3>
-                    <h4 className={'text-base font-bold'}>{occupation.organization}</h4>
-                    <p className={'mt-1text-white/50 text-xs'}>{occupation.display_date}</p>
-                    <ul className={'mt-4 list-disc list-inside pl-5'}>
-                        {occupation.description.split('\n').map((item) => (
-                            <li className={'mt-4 pl-5'}>{item}</li>
-                            ))}
-                    </ul>
-                </div>
-            ))}
+            <h3 className={'text-4xl pb-8 font-bold text-center'}>Timeline</h3>
+            <VerticalTimeline>
+                {timeline.map((occupation, index) => (
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(255, 255, 255)', color: '#000000' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)'}}
+                        iconStyle={{ background: 'transparent', boxShadow: 'none'}}
+                        icon = { <div className={'flex h-full w-full items-center justify-center'}>
+                                <div className={`h-8 w-8 rounded-full ${occupation.type === 'experience' ? 'bg-purple-500' : 'bg-blue-500'} border-4 border-white`}/>
+                            </div>}>
+                        <div key={index}>
+                            <h4 className={'text-xl mt-4 font-bold'}>{occupation.title}</h4>
+                            <p className={'text-base font-bold'}>{occupation.organization}</p>
+                            <p className={'mt-1 text-gray-400 text-xs!'}>{occupation.display_date}</p>
+                            <ul className={'mt-4 list-disc list-inside pl-5'}>
+                                {occupation.description.split('\n').map((item) => (
+                                    <li className={'mt-4 pl-5'}>{item}</li>
+                                    ))}
+                            </ul>
+                        </div>
+                  </VerticalTimelineElement>
+                ))}
+            </VerticalTimeline>
         </section>
     )
 }
