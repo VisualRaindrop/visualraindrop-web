58b99748be954548546049df4c5c63464b89618b
Kevin Truong <kevin97truong@gmail.com>
2026-05-13 08:03:39 -0700

    Add project pages and sections

---
+++ frontend/src/sections/Project_Introduction.tsx
@@ -0,0 +1,9 @@
+function Project_Introduction() {
+    return (
+        <section className={'flex flex-col md:flex-row md:justify-between items-center mx-auto'}>
+            <h1 className={'font-light text-4xl md:text-5xl xl:text-6xl 2xl:text-7xl'}>Projects</h1>
+        </section>
+    )
+}
+
+export default Project_Introduction;
