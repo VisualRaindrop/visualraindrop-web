a70ecaae76a499f61fac8e3d0848861f0f5d7273
Kevin Truong <kevin97truong@gmail.com>
2026-05-18 15:41:29 -0700

    Fix Mobile UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router"
 
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home"
-import Projects from "./pages/Projects";
 
 function App() {
     return (
@@ -10,7 +9,6 @@ function App() {
             <MainLayout>
                 <Routes>
                     <Route path={'/'} element={<Home />}/>
-                    <Route path={'/Projects'} element={<Projects />}/>
                 </Routes>
             </MainLayout>
         </BrowserRouter>
